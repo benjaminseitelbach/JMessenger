629c46c9c7d633e7d99a0f38a7f7f742e58ac954
benjooouille <benjamin.seitelbach@hotmail.fr>
2019-03-12 15:27:26 +0100

    Alois suce ma bite

--- base.sql
+++ base.sql
@@ -4,17 +4,22 @@ create database Jmessenger;
 use Jmessenger;
 
 create table utilisateurs ( pseudo VARCHAR(10), mdp VARCHAR(16) );
-create table salons( membre VARCHAR(10) );
+create table salons( nom VARCHAR(10) );
 
 INSERT INTO utilisateurs (pseudo, mdp) 
     VALUES ('benjo', 'oui');
-    
-DELETE FROM utilisateurs;
 
-UPDATE utilisateurs SET salon = 'Les BROKC' WHERE pseudo = 'Benjo';
+ALTER TABLE utilisateurs DROP nombreSalons;  
+ALTER TABLE utilisateurs ADD nombreSalons INT(1); 
+DELETE FROM utilisateurs;
+DELETE FROM salons;
+ALTER TABLE salons ADD nombreMembres INT(1);
+UPDATE utilisateurs SET nombreSalons = 0 WHERE pseudo = 'Alois';
 
 UPDATE utilisateurs SET mdp = 'a' WHERE pseudo = 'Benjo';
-
+UPDATE salons SET createur = 'Benjo' WHERE nom = 'Les BROKC';
+INSERT INTO salons (nom) VALUES ('Les BROKC');
+INSERT INTO salons (createur) VALUES ('Benjo');
 select * from utilisateurs;
 
 select * from salons;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -30,30 +30,13 @@ import java.util.ArrayList;
 
 public class Main extends Application {
 
-    static int numeroLigneSalon = 4;
     static String pseudo;
+    static int numeroLigneSalon;
 
     public static void debug () {
 
         //sql("ALTER TABLE utilisateurs ADD salon VARCHAR(50);");
         sql("UPDATE utilisateurs SET salon = 'Les BRO' WHERE pseudo = 'Benjo';");
-
-    }
-
-
-    public static void setSql (String sPseudo, String sMdp) {
-        try{
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/Jmessenger?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","root");
-
-            Statement stmt = con.createStatement();
-
-            stmt.executeUpdate("INSERT INTO utilisateurs (pseudo, mdp) VALUES ('"+sPseudo+"','"+sMdp+"')");
-
-            con.close();
-        }catch(Exception e){ System.out.println(e);}
 
     }
 
@@ -107,6 +90,49 @@ public class Main extends Application {
     }
 
     public static ArrayList<String> getSalons (String pseudo) {
+        ArrayList<String> salons = new ArrayList<String>();
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Jmessenger?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","root");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs=stmt.executeQuery("select * from utilisateurs");
+
+            while(rs.next()) {
+                System.out.println(rs.getString(1));
+                if (rs.getString(1).equals(pseudo)) {
+                    System.out.println("Pseudo trouve");
+                    ResultSetMetaData rsmd = rs.getMetaData();
+
+                    int columnsNumber = rsmd.getColumnCount();
+                    System.out.println("nombre de colonnes : "+columnsNumber);
+                    int colonne = 4;
+
+                    while(colonne <= columnsNumber) {
+                        System.out.println(rs.getString(colonne));
+                        salons.add(rs.getString(colonne));
+                        colonne ++;
+                    }
+                    return salons;
+
+                }
+
+            }
+
+            con.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+
+        }
+        return salons;
+    }
+
+    public static int getNombreSalons (String pseudo) {
+
         try{
 
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -119,14 +145,12 @@ public class Main extends Application {
 
             while(rs.next()) {
 
-                if (pseudo.equals(rs.getString(1))) {
-                    ArrayList<String> salons = new ArrayList<String>();
-                    int colonne = 1;
-                    while(!rs.getString(colonne).isEmpty()){
-                        salons.add(rs.getString(colonne+2));
-                    }
-                    con.close();
-                    return salons;
+                if (rs.getString(1).equals(pseudo)) {
+
+                    int nombreSalons = rs.getInt(3);
+
+                    return nombreSalons;
+
                 }
 
             }
@@ -135,15 +159,47 @@ public class Main extends Application {
 
         }catch(Exception e){
             System.out.println(e);
-            return new ArrayList<String>();
+
         }
-        return new ArrayList<String>();
+        return 0;
     }
+
+    public static int getNombreMembres (String salon) {
+
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Jmessenger?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","root");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs=stmt.executeQuery("select * from salons");
+
+            while(rs.next()) {
+
+                if (rs.getString(1).equals(salon)) {
+
+                    int nombreMembres = rs.getInt(3);
+
+                    return nombreMembres;
+
+                }
+
+            }
+
+            con.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+
+        }
+        return 0;
+    }
+
 
     @Override
     public void start(Stage pageConnexion) {
-
-
 
         //Initialisation du titre de la page connexion
         pageConnexion.setTitle("JMessenger");
@@ -196,12 +252,12 @@ public class Main extends Application {
         grillePrincipale.setVgap(10);
         grillePrincipale.setPadding(new Insets(25, 25, 25, 25));
 
+        //Bouton deconnexion
+        Button boutonDeconnexion = new Button ("Déconnexion");
+        grillePrincipale.add(boutonDeconnexion, 5, 0);
+
         Button boutonCreerSalon = new Button("Créer un salon");
         grillePrincipale.add(boutonCreerSalon, 0, 2);
-
-        //Salons disponibles
-        Label texteSalons = new Label("Salons: ");
-        grillePrincipale.add(texteSalons,0,3);
 
         /* CREATION D'UN SALON */
 
@@ -220,7 +276,9 @@ public class Main extends Application {
         texteNomSalonCreation.setVisible(false);
         boutonCreer.setVisible(false);
 
-
+        //Salons disponibles
+        Label texteSalons = new Label("Salons: ");
+        grillePrincipale.add(texteSalons,0,3);
 
         Scene scenePrincipale = new Scene(grillePrincipale, 800, 500);
 
@@ -237,29 +295,140 @@ public class Main extends Application {
                     Text scenetitle = new Text("Bonjour "+pseudo);
                     scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                     grillePrincipale.add(scenetitle, 0, 0, 2, 1);
-                    pagePrincipale.setScene(scenePrincipale);
-                    /*ArrayList<String> salons = getSalons(pseudo);
-                    for(int i = 0; i < salons.size(); i++) {
-                        System.out.println(salons.get(i));
+
+                    //Récupération des salons de l'utilisateur dans la base de données
+                    ArrayList<String> salons = getSalons(pseudo);
+
+                    //Parcours des salons
+                    for(int i = 0; i<salons.size(); i++ ){
+
+                        numeroLigneSalon = i + 4;
+                        String nomSalon = salons.get(i);
+
+                        if(nomSalon != null) {
+
+                            //Création d'un bouton sur la page principale
+                            Button boutonSalon = new Button(nomSalon);
+                            grillePrincipale.add(boutonSalon, 0, numeroLigneSalon);
+
+                            //Initialisation de la page du salon
+                            Stage pageSalon = new Stage();
+                            pageSalon.setTitle(nomSalon);
+
+                            GridPane grilleSalon = new GridPane();
+                            //grilleSalon.setAlignment(Pos.TOP_LEFT);
+                            grilleSalon.setHgap(15);
+                            grilleSalon.setVgap(1);
+
+                            grilleSalon.setPadding(new Insets(25, 0, 0, 0));
+
+                            Text texteNomSalon = new Text(nomSalon);
+                            texteNomSalon.setFont(Font.font ("Verdana", 20));
+                            grilleSalon.add(texteNomSalon, 1, 0);
+
+                            Button boutonVoirMembres = new Button("Voir membres");
+                            grilleSalon.add(boutonVoirMembres, 1, 3);
+
+                            Button boutonAjouterMembre = new Button("Ajouter un membre");
+                            grilleSalon.add(boutonAjouterMembre, 2, 3);
+
+                            Text textePseudoMembre = new Text("Pseudo du membre à ajouter:");
+                            grilleSalon.add(textePseudoMembre, 2, 3);
+
+                            TextField saisiePseudoMembre = new TextField();
+                            grilleSalon.add(saisiePseudoMembre, 2, 4);
+
+                            Button boutonAjouter = new Button ("Ajouter");
+                            grilleSalon.add(boutonAjouter, 2, 5);
+
+                            textePseudoMembre.setVisible(false);
+                            saisiePseudoMembre.setVisible(false);
+                            boutonAjouter.setVisible(false);
+
+                            Button boutonSupprimerMembre = new Button("Supprimer un membre");
+                            grilleSalon.add(boutonSupprimerMembre, 3, 3, 8, 1);
+
+                            Text textePseudoMembreSupprimer = new Text("Pseudo du membre à supprimer:");
+                            grilleSalon.add(textePseudoMembreSupprimer, 3, 3, 8, 1);
+
+                            TextField saisiePseudoMembreSupprimer = new TextField();
+                            grilleSalon.add(saisiePseudoMembreSupprimer, 3, 4, 8, 1);
+
+                            Button boutonSupprimer = new Button ("Supprimer");
+                            grilleSalon.add(boutonSupprimer, 3, 5, 8, 1);
+
+                            textePseudoMembreSupprimer.setVisible(false);
+                            saisiePseudoMembreSupprimer.setVisible(false);
+                            boutonSupprimer.setVisible(false);
+
+                            TextField saisieMessage = new TextField();
+                            //saisieMessage.setMinSize( 3, 30);
+                            grilleSalon.add(saisieMessage, 0, 400, 10, 1);
+
+                            Button boutonEnvoyerMessage = new Button("Envoyer");
+                            //boutonEnvoyerMessage.setMinSize(50,30);
+                            grilleSalon.add(boutonEnvoyerMessage, 10, 400);
+
+                            Scene sceneSalon= new Scene(grilleSalon, 500, 500);
+                            pageSalon.setScene(sceneSalon);
+
+                            boutonAjouterMembre.setOnAction(new EventHandler<ActionEvent>() {
+
+                                @Override
+                                public void handle(ActionEvent e) {
+                                    boutonAjouterMembre.setVisible(false);
+                                    textePseudoMembre.setVisible(true);
+                                    saisiePseudoMembre.setVisible(true);
+                                    boutonAjouter.setVisible(true);
+                                }
+                            });
+
+                            boutonAjouter.setOnAction(new EventHandler<ActionEvent>() {
+                                int numeroLigneMembre = 3;
+                                @Override
+                                public void handle(ActionEvent e) {
+                                    String nomMembre = saisiePseudoMembre.getText();
+
+                                    int nombreMembres = getNombreMembres(nomSalon);
+                                    System.out.println(nombreMembres);
+
+                                    sql("UPDATE salons SET membre"+nombreMembres+" = '"+nomMembre+"' WHERE nom = '"+nomSalon+"';");
+
+                                    sql("UPDATE salons SET nombreMembres = "+(nombreMembres+1)+" WHERE nom = '"+nomSalon+"';");
+
+                                    boutonAjouterMembre.setVisible(true);
+                                    textePseudoMembre.setVisible(false);
+                                    saisiePseudoMembre.setVisible(false);
+                                    boutonAjouter.setVisible(false);
+
+                                }
+                            });
+
+                            numeroLigneSalon++;
+
+                            boutonSalon.setOnAction(new EventHandler<ActionEvent>() {
+
+                                @Override
+                                public void handle(ActionEvent e) {
+                                    pageSalon.show();
+                                }
+                            });
+                        }
 
                     }
-                    */
+                    pagePrincipale.setScene(scenePrincipale);
                     pagePrincipale.show();
                     pageConnexion.close();
 
                 }
                 else {
-
                     final Text actiontarget = new Text();
                     actiontarget.setFill(Color.FIREBRICK);
                     actiontarget.setText("Pseudo ou mot de passe incorrect");
                     grilleConnexion.add(actiontarget, 0, 6);
-
                 }
-
             }
         });
-
 
         inscription.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -270,14 +439,25 @@ public class Main extends Application {
                 Utilisateur utilisateur = new Utilisateur(pseudo, mdp);
                 System.out.println(pseudo);
                 System.out.println(mdp);
-                setSql(pseudo, mdp);
-
+                //setSql(pseudo, mdp);
+                sql("INSERT INTO utilisateurs (pseudo, mdp, nombreSalons) VALUES ('"+pseudo+"','"+mdp+"',0);");
                 Text scenetitle = new Text("Bienvenue "+pseudo);
                 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 grillePrincipale.add(scenetitle, 0, 0, 2, 1);
                 pagePrincipale.setScene(scenePrincipale);
                 pagePrincipale.show();
                 pageConnexion.close();
+
+            }
+        });
+
+        boutonDeconnexion.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+
+                pagePrincipale.close();
+                pageConnexion.show();
 
             }
         });
@@ -302,11 +482,18 @@ public class Main extends Application {
 
                 String nomSalonCreation = saisieNomSalonCreation.getText();
 
-                sql("INSERT INTO salons (nom) VALUES ('"+nomSalonCreation+"');");
+                int numeroSalon = getNombreSalons(pseudo);
 
-                sql("ALTER TABLE salons ADD createur VARCHAR(10)");
+                //sql("ALTER TABLE utilisateurs ADD salon"+no+" VARCHAR(10);");
 
-                sql("UPDATE salons SET createur = '"+pseudo+"' WHERE nom = '"+nomSalonCreation+"';");
+
+                sql("UPDATE utilisateurs SET salon"+numeroSalon+" = '"+nomSalonCreation+"' WHERE pseudo = '"+pseudo+"';");
+
+                sql("INSERT INTO salons (nom, createur, nombreMembres) VALUES ('"+nomSalonCreation+"','"+pseudo+"',1);");
+
+                //sql("UPDATE salons SET createur = '"+pseudo+"' WHERE nom = '"+nomSalonCreation+"';");
+
+                sql("UPDATE utilisateurs SET nombreSalons = "+(numeroSalon+1)+" WHERE pseudo = '"+pseudo+"';");
 
                 //Initialisation de la page du salon
                 Stage pageSalon = new Stage();
@@ -369,6 +556,28 @@ public class Main extends Application {
                 Scene sceneSalon= new Scene(grilleSalon, 500, 500);
                 pageSalon.setScene(sceneSalon);
 
+                boutonVoirMembres.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent e) {
+                        Stage pageMembresSalon = new Stage();
+
+                        pageMembresSalon.setTitle("Membres "+nomSalonCreation);
+                        GridPane grilleMembresSalon = new GridPane();
+                        grilleMembresSalon.setAlignment(Pos.TOP_LEFT);
+                        grilleMembresSalon.setHgap(10);
+                        grilleMembresSalon.setVgap(10);
+                        grilleMembresSalon.setPadding(new Insets(25, 25, 25, 25));
+                        Text texteMembres = new Text("Membres "+nomSalonCreation);
+                        grilleMembresSalon.add(texteMembres, 0, 0);
+
+                        Scene sceneMembresSalon = new Scene(grilleMembresSalon, 200, 200);
+                        pageMembresSalon.setScene(sceneMembresSalon);
+
+                        pageMembresSalon.show();
+                    }
+                });
+
                 boutonAjouterMembre.setOnAction(new EventHandler<ActionEvent>() {
 
                     @Override
@@ -385,13 +594,25 @@ public class Main extends Application {
                     @Override
                     public void handle(ActionEvent e) {
                         String nomMembre = saisiePseudoMembre.getText();
+
+                        int nombreMembres = getNombreMembres(nomSalonCreation);
+                        System.out.println(nombreMembres);
+
+                        sql("UPDATE salons SET membre"+nombreMembres+" = '"+nomMembre+"' WHERE nom = '"+nomSalonCreation+"';");
+
+                        sql("UPDATE salons SET nombreMembres = "+(nombreMembres+1)+" WHERE nom = '"+nomSalonCreation+"';");
+
                         boutonAjouterMembre.setVisible(true);
                         textePseudoMembre.setVisible(false);
                         saisiePseudoMembre.setVisible(false);
                         boutonAjouter.setVisible(false);
+
+
+                        /*
                         Text texteNomMembre = new Text(nomMembre);
                         grilleSalon.add(texteNomMembre, 1, numeroLigneMembre);
                         numeroLigneMembre ++;
+                        */
                     }
                 });
 
